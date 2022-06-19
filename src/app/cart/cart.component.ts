121919e20f0dfedf68a7bf194f3ac4e2ddd32ac0
kotasudhakar <kotasudhakar2010@gmail.com>
2022-06-19 20:27:06 +0530

    Update cart.component.ts

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -4,7 +4,7 @@ import { CartService } from 'src/app/service/cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss'],
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
   public products: any = [];
